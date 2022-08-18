35eee42264f1e78cbe25d4d959141be98d608bff
Ronald Seah <ronseahald@hotmail.com>
2022-08-18 21:48:13 +0800

    Add constructors for Aes256GcmCrypt

--- src/crypt.rs
+++ src/crypt.rs
@@ -1,4 +1,5 @@
 use aes_gcm::{aead::Aead, Aes256Gcm, KeyInit};
+use rand::{rngs::OsRng, RngCore};
 
 pub enum CryptError {
     EncryptionError,
@@ -6,15 +7,27 @@ pub enum CryptError {
 }
 
 pub type Aes256KeyBytes = [u8; 32];
+pub type Aes256GcmNonce = [u8; 12];
 
 
 /// A struct to assist with encrypting and decrypting with AES-256-GCM. Uses RustCrypto's
 /// `aes_gcm` crate
 pub struct Aes256GcmCrypt {
-    nonce: [u8; 12],
+    nonce: Aes256GcmNonce,
     key: Aes256KeyBytes,
 }
 
+impl Aes256GcmCrypt {
+    pub fn new(key: Aes256KeyBytes) -> Aes256GcmCrypt {
+        let mut nonce: Aes256GcmNonce = [];
+        OsRng.fill_bytes(&nonce);
+        Aes256GcmCrypt { nonce: nonce, key: key }
+    }
+
+    pub fn from_nonce(key: Aes256KeyBytes, nonce: Aes256GcmNonce) -> Aes256GcmCrypt {
+        Aes256GcmCrypt { nonce: nonce, key: key }
+    }
+}
 
 pub trait Crypt {
     fn encrypt(&self, plaintext: &str) -> Result<String, CryptError>;
