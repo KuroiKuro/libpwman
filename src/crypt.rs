e98767c6c7134efd1383445a377a0cac7d3e2436
Ronald Seah <ronseahald@hotmail.com>
2022-08-19 21:49:29 +0800

    Make changes

--- src/crypt.rs
+++ src/crypt.rs
@@ -1,5 +1,7 @@
-use aes_gcm::{aead::Aead, Aes256Gcm, KeyInit};
+use aes_gcm::{aead::Aead, Aes256Gcm, KeyInit, Nonce};
 use rand::{rngs::OsRng, RngCore};
+use generic_array::GenericArray;
+use std::str;
 
 pub enum CryptError {
     EncryptionError,
@@ -19,8 +21,8 @@ pub struct Aes256GcmCrypt {
 impl Aes256GcmCrypt {
     /// Create a new instance of Aes256GcmCrypt with a new randomly generated nonce
     pub fn new(key: Aes256KeyBytes) -> Aes256GcmCrypt {
-        let mut nonce: Aes256GcmNonce = [];
-        OsRng.fill_bytes(&nonce);
+        let mut nonce: Aes256GcmNonce = [0; 12];
+        OsRng.fill_bytes(&mut nonce);
         Aes256GcmCrypt {
             nonce: nonce,
             key: key,
@@ -38,25 +40,63 @@ impl Aes256GcmCrypt {
 
 /// This trait provides an interface for structs to provide encryption and decryption operations
 pub trait Crypt {
-    fn encrypt(&self, plaintext: &str) -> Result<String, CryptError>;
-    fn decrypt(&self, ciphertext: &str) -> Result<String, CryptError>;
+    fn encrypt(&self, plaintext: &str) -> Result<Vec<u8>, CryptError>;
+    fn decrypt(&self, ciphertext: &[u8]) -> Result<Vec<u8>, CryptError>;
 }
 
 impl Crypt for Aes256GcmCrypt {
     /// Encrypts a plaintext with AES-256-GCM
-    fn encrypt(&self, plaintext: &str) -> Result<String, CryptError> {
-        let cipher = Aes256Gcm::new(&self.key);
-        match cipher.encrypt(&self.nonce, plaintext) {
-            Ok(result) => Ok(result.into()),
+    fn encrypt(&self, plaintext: &str) -> Result<Vec<u8>, CryptError> {
+        let key = GenericArray::from_slice(&self.key);
+        let cipher = Aes256Gcm::new(key);
+        let nonce = Nonce::from_slice(&self.nonce);
+
+        match cipher.encrypt(nonce, plaintext.as_bytes()) {
+            Ok(result) => Ok(result),
             Err(_) => Err(CryptError::EncryptionError),
         }
     }
 
-    fn decrypt(&self, ciphertext: &str) -> Result<String, CryptError> {
-        let cipher = Aes256Gcm::new(&self.key);
-        match cipher.decrypt(&self.nonce, ciphertext) {
-            Ok(result) => Ok(result.into()),
+    fn decrypt(&self, ciphertext: &[u8]) -> Result<Vec<u8>, CryptError> {
+        let key = GenericArray::from_slice(&self.key);
+        let cipher = Aes256Gcm::new(key);
+        let nonce = Nonce::from_slice(&self.nonce);
+
+        match cipher.decrypt(nonce, ciphertext) {
+            Ok(result) => Ok(result),
             Err(_) => Err(CryptError::DecryptionError),
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use pbkdf2::password_hash::SaltString;
+    use super::{Aes256KeyBytes, Aes256GcmCrypt, Crypt};
+    use crate::keys::{generate_salt, get_key_bytes_from_pw};
+
+    fn generate_key() -> (Aes256KeyBytes, SaltString) {
+        let salt = generate_salt();
+        let key = get_key_bytes_from_pw("password", &salt);
+        (key, salt)
+    }
+
+    #[test]
+    fn test_aes256gcmcrypt_crypt() {
+        let (key, salt) = generate_key();
+        let crypt = Aes256GcmCrypt::new(key);
+
+        // Test that after encryption and decryption, we get the same value
+        let plaintext = "I am Malenia, Blade of Miquella";
+        let ciphertext = match crypt.encrypt(plaintext) {
+            Ok(ciphertext) => ciphertext,
+            Err(_) => panic!("Encryption encountered an error")
+        };
+        let decrypted_ciphertext = match crypt.decrypt(&ciphertext) {
+            Ok(text) => text,
+            Err(_) => panic!("Decryption encountered an error")
+        };
+        assert_eq!(plaintext.as_bytes(), decrypted_ciphertext);
     }
 }
