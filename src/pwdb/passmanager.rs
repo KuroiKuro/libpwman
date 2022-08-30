ba96ddfdb76a45efc6938550a310b67c5d4e7504
Ronald Seah <ronseahald@hotmail.com>
2022-08-30 18:09:23 +0800

    WIP for PassEntryManager

--- src/pwdb/passmanager.rs
+++ src/pwdb/passmanager.rs
@@ -1,6 +1,6 @@
 //! This module provides the functionality for the password manager
 
-use crate::pwdb::passentry::PasswordEntry;
+use crate::pwdb::passentry::{PassEntry, PasswordEntry};
 
 /// A handler type that handles saving and retrieving of the various password entries.
 ///
@@ -12,9 +12,67 @@ pub struct PassEntryManager<T: PasswordEntry> {
     entries: Vec<T>,
 }
 
+impl PassEntryManager<PassEntry> {
+    pub fn new() -> PassEntryManager<PassEntry> {
+        PassEntryManager { entries: Vec::new() }
+    }
+
+    pub fn set_entries(&mut self, entries: Vec<PassEntry>) {
+        self.entries = entries;
+    }
+}
+
 pub trait PasswordEntryManager {
     type PasswordEntryType;
 
     fn get_all_entries(&self) -> Vec<Self::PasswordEntryType>;
-    fn filter_by_field(&self, field_name: &str, field_value: &str);
+    fn filter_by_title(&self, search_value: &str, case_sensitive: bool) -> Vec<&Self::PasswordEntryType>;
+}
+
+impl PasswordEntryManager for PassEntryManager<PassEntry> {
+    type PasswordEntryType = PassEntry;
+
+    fn get_all_entries(&self) -> Vec<Self::PasswordEntryType> {
+        self.entries.to_vec()
+    }
+
+    fn filter_by_title(&self, search_value: &str, case_sensitive: bool) -> Vec<&Self::PasswordEntryType>{
+        let mut ret_vec: Vec<&Self::PasswordEntryType> = Vec::new();
+        for entry in &self.entries {
+            let title = match &entry.title {
+                Some(title) => title,
+                None => continue
+            };
+            let title = String::from(title);
+
+            let title_to_search = if !case_sensitive {
+                title.to_lowercase()
+            } else {title};
+
+            if title_to_search.contains(search_value) {
+                ret_vec.push(&entry);
+            }
+        }
+        ret_vec
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::keys::{generate_salt, get_key_bytes_from_pw, Aes256KeyBytes, SaltString};
+    use crate::pwdb::{PassEntry, PasswordEntry};
+
+    fn create_key() -> (Aes256KeyBytes, SaltString) {
+        let db_password = "password";
+        let salt = generate_salt();
+        let key = get_key_bytes_from_pw(db_password, &salt);
+        (key, salt)
+    }
+
+    #[test]
+    fn test_get_all_entries() {
+        let password1 = PassEntry::new();
+        password1.title = "title1";
+        password1.save_password("password1", enc_key)
+    }
 }
