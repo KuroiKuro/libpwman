d710c159304ade5f354e69fbb4231179d2a49ac4
Ronald Seah <ronseahald@hotmail.com>
2022-09-09 21:35:14 +0800

    Create write_data function

--- src/dbfile/fileserial.rs
+++ src/dbfile/fileserial.rs
@@ -1,5 +1,16 @@
+use std::io::Write;
+use bincode;
+
 #[derive(Debug)]
 pub enum DbFileWriteError {
     FileCreateError,
     FileWriteError,
+}
+
+
+pub fn write_data(writer: &mut dyn Write, data: &[u8]) -> Result<(), DbFileWriteError> {
+    if let Err(_) = bincode::serialize_into(writer, &data) {
+        return Err(DbFileWriteError::FileWriteError);
+    };
+    Ok(())
 }
