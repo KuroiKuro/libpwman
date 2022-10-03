7766e0457a4a82ddff23e434830ef66a2a730ddc
Ronald Seah <ronseahald@hotmail.com>
2022-10-03 18:59:04 +0800

    Create function to generate plaintext

--- src/dbfile/utils.rs
+++ src/dbfile/utils.rs
@@ -1,4 +1,6 @@
 use crc::{Crc, CRC_32_CKSUM};
+use rand::RngCore;
+use rand::rngs::OsRng;
 
 pub const CHECKSUM_SIZE_BYTES: u8 = 4;
 const CRC32_CKSUM_ALG: Crc<u32> = Crc::<u32>::new(&CRC_32_CKSUM);
@@ -10,9 +12,22 @@ pub fn calculate_crc32(data: &[u8]) -> u32 {
     digest.finalize()
 }
 
+/// Initializes a vector with a specific number of placeholder values
 pub fn initialize_vec(v: &mut Vec<u8>, value: u8, iterations: usize) -> &Vec<u8> {
     for _ in 0..iterations {
         v.push(value);
     }
     v
+}
+
+/// Generates a random plaintext data slice, for use in the db file header. The plaintext will
+/// be encrypted and the resulting ciphertext can be used to test if a user has entered a correct
+/// password. If the password is correct, the decrypted ciphertext should match the plaintext.
+/// The generated plaintext is not actually text, just a sequence of bytes. It is generated with
+/// the `OsRng` struct from the `rand` crate
+pub fn generate_plaintext_check(plaintext_length: usize) -> Vec<u8> {
+    let mut plaintext: Vec<u8> = vec![0; plaintext_length];
+    let rng = OsRng::default();
+    rng.fill_bytes(&mut plaintext);
+    plaintext
 }
